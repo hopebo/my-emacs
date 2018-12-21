90cadbd2faf4b8b1818d3e836c80266d0f7ad5b6
Hope <hope@admindeMacBook-Pro.local>
2018-12-21 11:17:38 +0800

    Initialize my emacs config
    - Use org-mode file to manage emacs config.
    - Add some basic settings.

--- init.el
+++ init.el
@@ -1,7 +1,1 @@
-;; Load solarized dark theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/site-lisp/emacs-color-theme-solarized")
-(load-theme 'solarized t)
-(set-terminal-parameter nil 'background-mode 'dark)
-(enable-theme 'solarized)
-;; Remove extra whitespaces
-(add-hook 'before-save-hook (lambda () (whitespace-cleanup)))
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
