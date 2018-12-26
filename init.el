c388be98a1834bf38a6e311a0d81760259851916
hopebo <hopelee1994@gmail.com>
2018-12-26 17:44:23 +0800

    Add Yasnippet, Iedit, Web Mode and Git related plugins
    - Yasnippet: multiple templates for coding and writing
    - Iedit: edit places simultaniously
    - Web Mode: web related support
    - Magit: git operations inside emacs
    - Git gutter: quickly jump between hunks and choose stage or revert them
    - Git timemachine: switch among different commits
    - Add custom file to store customized values

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
 (package-initialize)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+(setq custom-file "~/.emacs.d/site-lisp/custom.el")
+(load custom-file)
 (provide 'init)
+;;; init.el ends here
---
+++ site-lisp/custom.el
@@ -0,0 +1,7 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(iedit-occurrence ((t (:inherit nil :background "white")))))
+;;; custom.el ends here
