7bbbfb444a245ab15eccd9dc13e2c4ed6dbcc2a5
hopebo <hopelee1994@gmail.com>
2018-12-27 17:38:23 +0800

    Add some plugins, do adjustments on the face
    - Multiple Cursors: edit multiple places simultaniously with multiple
      cursors
    - Treemacs: display folders like a tree
    - Text Scale: text scaling
    - Smartparens: display parenthesis smartly
    - Projectile: manage project files
    - Origami: text folding
    - Dumb Jump: help quickly find symbol definitions
    - Add Ansi-term customize settings
    - Hydra: plugin to help with keybinding
    - Remove multi-term
    - Do some adjustments on the face
    - Manually smart-mode-line.el and smart-mode-line-respectful-theme.el for
      a nice face of mode line. Automatic loading smart-mode-line using use-package
      doesn't work well for me.
    - Add browse-kill-ring.el for browsing kill ring in a buffer.

--- init.el
+++ init.el
@@ -1,7 +1,11 @@
+;;; package --- summary: My emacs configuration
+
 (package-initialize)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
 (setq custom-file "~/.emacs.d/site-lisp/custom.el")
-(load custom-file)
+;; Load custom file only if it exists
+(if (file-readable-p custom-file) (load custom-file))
+
 (provide 'init)
 ;;; init.el ends here
