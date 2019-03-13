41a478c829c9e0c29ac21ae7e0b5d1f1c612ff9d
hopebo <hopelee1994@gmail.com>
2019-03-13 15:42:42 +0800

    Make indentation visible and flycheck style changes
    - Make cscope-program changed to cscope.
    - Add org-preview-html mode.
    - Flycheck, iedit-occurrence and indentation style changes.

--- site-lisp/custom.el
+++ site-lisp/custom.el
@@ -3,5 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(iedit-occurrence ((t (:inherit nil :background "white")))))
+ '(flycheck-error ((t (:foreground "color-125" :underline t))))
+ '(iedit-occurrence ((t (:inherit nil :background "white"))))
+ '(whitespace-indentation ((t (:foreground "cyan")))))
 ;;; custom.el ends here
