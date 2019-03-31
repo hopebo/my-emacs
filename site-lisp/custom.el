fd12608128f67c87431b9871f8ef73263cc96807
hopebo <hopelee1994@gmail.com>
2019-03-31 22:50:58 +0800

    Refactor coding mode settings, add some org-mode settings
    - Refactor codes, only let specific coding modes load some settings.
    - Add some org-mode related things, like GTD.

--- site-lisp/custom.el
+++ site-lisp/custom.el
@@ -5,5 +5,6 @@
  ;; If there is more than one, they won't work right.
  '(flycheck-error ((t (:foreground "color-125" :underline t))))
  '(iedit-occurrence ((t (:inherit nil :background "white"))))
+ '(org-todo ((t (:background "red" :foreground "cyan" :weight bold))))
  '(whitespace-indentation ((t (:foreground "cyan")))))
 ;;; custom.el ends here
