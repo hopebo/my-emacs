dd18fe349cf1d78b45f6f7b84c590c9b356cd94e
hopebo <hopelee1994@gmail.com>
2019-04-21 11:02:26 +0800

    Add gdb related configurations

--- site-lisp/custom.el
+++ site-lisp/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flycheck-error ((t (:foreground "color-125" :underline t))))
+ '(flycheck-error ((t (:underline t))))
  '(iedit-occurrence ((t (:inherit nil :background "white"))))
  '(org-todo ((t (:background "red" :foreground "cyan" :weight bold))))
  '(whitespace-indentation ((t (:foreground "cyan")))))
