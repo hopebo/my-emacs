63968e8f827d879caf18c6a7dfd7873044ff1560
李博 <libo@L-20LYLVDL-1611.local>
2020-02-08 20:17:52 +0800

    Add org gtd related settings

--- site-lisp/custom.el
+++ site-lisp/custom.el
@@ -8,3 +8,12 @@
  '(org-todo ((t (:background "red" :foreground "cyan" :weight bold))))
  '(whitespace-indentation ((t (:foreground "cyan")))))
 ;;; custom.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+
+ ;; Workaround way to solve that emacsclient cannot load theme
+ ;; configuration in init.el.
+ '(frame-background-mode (quote dark)))
